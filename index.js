3ae750cb5ab8d92cfff6ce74bdc17b3eb7be8617
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-09-29 13:17:02 +0200

    moved all files into JSL09 folder

---
+++ index.js
@@ -0,0 +1,37 @@
+try {
+    const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    const data = await res.json()
+    document.body.style.backgroundImage = `url(${data.urls.regular})`
+    document.getElementById("author").textContent = `By: ${data.user.name}`
+} catch (err) {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+    document.getElementById("author").textContent = `By: Dodi Achmad`
+}
+
+/**
+ * Challenge: Update the code below and in the 
+ * getCurrentLocation callback to use try...catch
+ */
+// use fetch and then instead of try and catch from line 17 - 42
+
+
+setInterval(getCurrentTime, 1000)
+
+navigator.geolocation.getCurrentPosition(async position => {
+    try {
+        const res = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+        if (!res.ok) {
+            throw Error("Weather data not available")
+        }
+        const data = await res.json()
+        const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        document.getElementById("weather").innerHTML = `
+            <img src=${iconUrl} />
+            <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+            <p class="weather-city">${data.name}</p>
+        `
+    } catch (err) {
+        console.error(err)
+    }
+});
